ffe2d657e25ca5fa2d01294b2f2cc18afa1bb06a
Paul Mason <paul@threesacompany.com>
2009-10-15 03:13:32 +0000

    Added another sample program for use in my blog: http://blog.paul-mason.co.nz/

---
+++ src/samples/HackableProgram/InvalidRegistrationKeyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HackableProgram
+{
+    [Serializable]
+    public class InvalidRegistrationKeyException : Exception
+    {
+        public InvalidRegistrationKeyException() { }
+        public InvalidRegistrationKeyException(string message) : base(message) { }
+        public InvalidRegistrationKeyException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidRegistrationKeyException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ src/samples/HackableProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using HackableProgram.Properties;
+
+namespace HackableProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            //First perform a check of the licensing key
+            try
+            {
+                CheckRegistrationDetails();
+            }
+            catch (InvalidRegistrationKeyException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+
+            //Display the welcome message
+            Console.WriteLine("Welcome to our program!");
+        }
+
+        /// <summary>
+        /// Checks the registration details.
+        /// </summary>
+        private static void CheckRegistrationDetails()
+        {
+            string value = ConfigurationManager.AppSettings["RegKey"];
+            DateTime expiryDate;
+            if (!DateTime.TryParse(value, out expiryDate))
+                throw new InvalidRegistrationKeyException(Resources.InvalidRegistrationKey);
+            if (DateTime.Now > expiryDate)
+                throw new InvalidRegistrationKeyException(Resources.InvalidRegistrationKey);
+            //We're ok
+        }
+    }
+}
